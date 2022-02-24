17c79eb71a827f563266b5caaebe06baafdef7a7
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-24 01:34:15 -0500

    Re-factor store configuration

--- IDHIStore.KKS/Store.cs
+++ IDHIStore.KKS/Store.cs
@@ -3,6 +3,7 @@
 //
 
 using KKAPI.MainGame;
+using KKAPI.Utilities;
 
 
 namespace IDHIPlugins
@@ -15,11 +16,28 @@ namespace IDHIPlugins
         {
             static internal void Init()
             {
-                StoreApi.RegisterShopItem(StoreItemId, "H Levels", "Gives access to H poses" +
-                    " independent of heroine experience there are three levels.  Firs level for" +
-                    " experience 50 second for 100 experience and third (sorry no more space)",
-                    StoreApi.ShopType.Normal, StoreApi.ShopBackground.Yellow, 3, 3, false, 200,
-                        numText: "{0} available upgrades");
+                var icon = ResourceUtils.GetEmbeddedResource(
+                    "hlevel_item.png", 
+                    typeof(IDHIStore).Assembly).LoadTexture();
+                var helvelCategoryId = StoreApi.RegisterShopItemCategory(icon);
+
+                StoreApi.RegisterShopItem(
+                    itemId: StoreItemId, 
+                    itemName: "H Experience Levels", 
+                    explaination:"Gives access to H poses independent of heroine experience there are " 
+                        + "three levels.  Firs level for experience 50 second for experience " 
+                        + "100 and third (sorry no more space)",
+#if DEBUG
+                    shopType: StoreApi.ShopType.Normal,
+#else
+                    shopType: StoreApi.ShopType.NightOnly,
+#endif
+                    itemBackground: StoreApi.ShopBackground.Pink,
+                    itemCategory: helvelCategoryId,
+                    stock: 3, 
+                    resetsDaily: false, 
+                    cost: 200,
+                    numText: "{0} available upgrades");
             }
 
             static public int GetHLevel()
