af81bf571eb66847660953162d30baaf9ec8c23e
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-27 02:48:37 -0500

    Update metadata

--- IDHIStore.KKS/Properties/AssemblyInfo.cs
+++ IDHIStore.KKS/Properties/AssemblyInfo.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/IDontHaveIdea/IDHIStore.git")]
+[assembly: AssemblyCopyright("Copyright ©IllusionMods  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("dc735533-276c-41b7-a882-77210c45cff1")]
