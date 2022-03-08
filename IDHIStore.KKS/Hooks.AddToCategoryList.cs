07f873601eb5486602f223d1cf9398c7ad2b9613
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-03-08 00:30:39 -0500

    Cleanup

--- IDHIStore.KKS/Hooks.AddToCategoryList.cs
+++ IDHIStore.KKS/Hooks.AddToCategoryList.cs
@@ -97,8 +97,6 @@ namespace IDHIPlugins
                     .GetComponentsInChildren<HPointData>(true);
                 var component = gameObjectList[gameObjectList.Count - 1]
                     .GetComponent<HPointOmitObject>();
-                //var flagAdditionalPosesRange1 = lstInitCategory.Any(c => c == 12 || c >= 1000);
-                //var flag3PRange2 = lstInitCategory.Any(c => c >= 3000 && c < 4000);
                 var hPointDataArray = componentsInChildren;
 
                 for (var index1 = 0; index1 < hPointDataArray.Length; ++index1)
@@ -114,104 +112,21 @@ namespace IDHIPlugins
                     msg.Clear();
                     if (!component.list.Contains(hPointData.gameObject))
                     {
-                        // Enable all animations like in FreeH for MainGame
-                        //if (!hpointData.category.Any((int c)
-                        //        => MathfEx.IsRange(2000, c, 2999, true))
-                        //    && IsCategoryInAnimationList(hsceneTraverse, hpointData.category)
-                        //    && IsExperience(hsceneTraverse, hpointData.Experience))
-                        //{
-                            /*msg.Append("GetCloseCategoryPostfix");
-                            SetCategoryXX(
-                                hsceneTraverse,
-                                hpointData,
-                                ref useCategorys,
-                                false,
-                                msg.ToString());*/
-                            var category = hPointData.category[0];
-                            if (!useCategorys.Contains(category))
+                        var category = hPointData.category[0];
+                        if (!useCategorys.Contains(category))
+                        {
+                            useCategorys.Add(category);
+                        }
+                        if (nowHpointData == null || !(hPointData.name == nowHpointData))
+                        {
+                            if (!closeHpointData.Contains(hPointData))
                             {
-                                //lines.Append($"Add to useCategorys {category}\n");
-                                useCategorys.Add(category);
+                                closeHpointData.Add(hPointData);
                             }
-                            if (nowHpointData == null || !(hPointData.name == nowHpointData))
-                            {
-                                if (!closeHpointData.Contains(hPointData))
-                                {
-                                    closeHpointData.Add(hPointData);
-                                }
-                            }
-                        //}
+                        }
                     }
                 }
             }
-
-            /*static private void SetCategoryXX(Traverse hsceneTraverse,
-                HPointData _data,
-                ref List<int> useCategorys,
-                bool _isSpecial = false,
-                string msg = "")
-            {
-                var lstInitCategory = hsceneTraverse
-                    .Field<List<int>>("lstInitCategory").Value;
-                var flags = hsceneTraverse
-                    .Field<HFlag>("flags").Value;
-
-                var flag3P = lstInitCategory.Any((int c) => c >= 3000 && c < 4000);
-
-                for (var i = 0; i < _data.category.Length; i++)
-                {
-                    var category = _data.category[i];
-                    //if ((!flags.isFreeH || IsCategoryInAnimationList(hsceneTraverse, num))
-                    if ((IsCategoryInAnimationList(hsceneTraverse, category))
-                        && (!_isSpecial || ((category >= 1000 || category == 12)
-                        && !((!flag3P) ? (!lstInitCategory.Contains(category))
-                            : (category < 3000 || category >= 4000))))
-                        && !useCategorys.Contains(category))
-                    {
-                        useCategorys.Add(category);
-                        _Log.Debug($"0008: [{msg}] Add category {(PositionCategory)category}");
-                    }
-                }
-            }
-
-            static private bool IsCategoryInAnimationList(Traverse hsceneTraverse, int[] _categorys)
-            {
-                var categorys = hsceneTraverse
-                    .Field<List<int>>("categorys").Value;
-
-                if (categorys.Any((int c) => c >= 1010 && c < 1100))
-                {
-                    return _categorys.Any((int c) => MathfEx.IsRange(1010, c, 1099, true));
-                }
-                if (categorys.Any((int c) => c >= 1100 && c < 1200))
-                {
-                    return _categorys.Any((int c) => MathfEx.IsRange(1100, c, 1199, true));
-                }
-                return true;
-            }
-
-            static private bool IsCategoryInAnimationList(Traverse hsceneTraverse, int _category)
-            {
-                var categorys = hsceneTraverse
-                    .Field<List<int>>("categorys").Value;
-
-                if (categorys.Any((int c) => c >= 1010 && c < 1100))
-                {
-                    return MathfEx.IsRange(1010, _category, 1099, true);
-                }
-                return !categorys.Any((int c) => c >= 1100 && c < 1200)
-                    || MathfEx.IsRange(1100, _category, 1199, true);
-            }
-
-            static private bool IsExperience(Traverse hsceneTraverse, int _experience)
-            {
-                var flags = hsceneTraverse
-                    .Field<HFlag>("flags").Value;
-
-                var hexperience = flags.lstHeroine[0].HExperience;
-                return _experience == 0
-                    || (_experience == 1 && hexperience > SaveData.Heroine.HExperienceKind.不慣れ);
-            }*/
         }
     }
 }
