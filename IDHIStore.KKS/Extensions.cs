e1cb7b4e2454792c1091aaf855284fd4fc4e7c92
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-04-19 11:11:14 -0400

    Extensions for PInfo class

---
+++ IDHIStore.KKS/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using IDHIUtils;
+
+namespace IDHIPlugins
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Get _alDicExpAddTaii through reflection
+        /// </summary>
+        /// <param name="pInfo"></param>
+        /// <returns></returns>
+        public static Dictionary<string, Dictionary<int, Dictionary<string, int>>> GetExpAddTaii(this Utilities.PInfo pInfo)
+        {
+            var _alDicExpAddTaii = pInfo.Traverse
+                    .Field<Dictionary<string,
+                        Dictionary<int, Dictionary<string, int>>>>("_alDicExpAddTaii").Value;
+            return _alDicExpAddTaii;
+        }
+
+        /// <summary>
+        /// Get string constructed for the Animation key through reflection
+        /// </summary>
+        /// <param name="pInfo"></param>
+        /// <param name="anim"></param>
+        /// <returns></returns>
+        public static string GetAnimationKey(this Utilities.PInfo pInfo, HSceneProc.AnimationListInfo anim)
+        {
+            var tmp = "";
+            try
+            {
+                var animationInfo = pInfo.Traverse
+                        .Method("GetAnimationKey",
+                            new Type[] { typeof(HSceneProc.AnimationListInfo), typeof(bool) });
+                if(animationInfo != null)
+                {
+                    tmp = animationInfo?.GetValue<string>(anim, true);
+                }
+            }
+            catch(Exception ex)
+            {
+                IDHIStoreItems._Log.Error($"Error: {ex.Message}");
+            }
+
+            return tmp;
+        }
+    }
+}
