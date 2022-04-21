1edbd7dfc27ddbd3c478bd9ce8f8b40cfc2ad498
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-04-21 02:35:19 -0400

    Bump version

--- IDHIStore.KKS/Plugin.Info.cs
+++ IDHIStore.KKS/Plugin.Info.cs
@@ -27,7 +27,7 @@ namespace IDHIPlugins
     public partial class IDHIStoreItems
     {
         public const string GUID = "com.ihavenoidea.idhistore";
-        public const string Version = "1.0.0.0";
+        public const string Version = "1.0.0.1";
 #if DEBUG
         public const string PluginDisplayName = "IDHI Store Items (Debug)";
 #else
